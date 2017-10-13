eb78d862e51b6cb4386a7d0a968b9775ed484408
chris-malloy <chrismalloymusic@gmail.com>
2017-10-13 09:49:42 -0400

    add comment

--- scripts.js
+++ scripts.js
@@ -1,36 +1,22 @@
-// 1. Give the user the ability to send a stock symbol.
-// 2. Ge the Symbol
-// 3. Once submitted, make an AJAX request to Yahoo
-// 4. Get the response from Yahoo and update the DOM
 $(document).ready(function() {
-    // setItem takes 2 args:
-    // 1. Name of the var  
-    // 2. Name to set
     var watchList = [
-            "goog",
-            "msft",
-            "tsla",
-            "tata",
-            "race"
-        ]
-        // Native javascript has JSON.stringify to turn a JSON object to a string/ necessary to do so browser can local store it 
+        "goog",
+        "msft",
+        "tsla",
+        "tata",
+        "race"
+    ]
     var watchListAsAString = JSON.stringify(watchList);
-    // Now you need to turn it back into an object to be used as JSON object so you can loop through it and make use of it again
-    // console.log(watchListAsAString)
     var watchListAsAnObjectAgain = JSON.parse(watchListAsAString);
-    // console.log(watchListAsAnObjectAgain)
-    // localStorage.setItem('watchList', "race");
     var watchList = localStorage.getItem('watchList');
     if (watchList != null) {
         updateWatchList();
+    } else {
+
     }
     var firstView = true;
     $('.yahoo-finance-form').submit((event) => {
         event.preventDefault();
-        // console.log("User submitted the form!")
-        // tell JSON two things
-        // 1. Where  
-        // 2. What to do (function)  
         var stockSymbol = $('#ticker').val();
         var url = 'http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + stockSymbol + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
         $.getJSON(url, (theDataJSFound) => {
@@ -51,20 +37,18 @@ $(document).ready(function() {
                 $('#stock-table-body').append(newRow);
             }
             $('td button').click(function() {
-                // add a click listener to all the buttons in the tables when clicked
-                // when clicked on, save the symbol to localStorage.
                 var stockToSave = $(this).attr('symbol');
                 var oldWatchList = localStorage.getItem('watchList');
-                // oldWatchList just came out of localStorage.
+                console.log(oldWatchList)
                 var oldAsJSON = JSON.parse(oldWatchList);
                 if (oldAsJSON == null) {
                     oldAsJSON = [];
                 }
                 if (oldAsJSON.indexOf(stockToSave) > -1) {
-
+                    console.log('no stock')
                 } else {
                     oldAsJSON.push(stockToSave);
-                    var newWatchListAsString = JSON.stringify(oldAsJSON)
+                    var newWatchListAsString = JSON.stringify(oldAsJSON);
                     localStorage.setItem('watchList', newWatchListAsString);
                     updateWatchList();
                 }
@@ -97,7 +81,6 @@ $(document).ready(function() {
         var watchList = localStorage.getItem('watchList');
         var watchListAsJSON = JSON.parse(watchList);
         watchListAsJSON.map((symbol, index) => {
-            console.log(symbol);
             var url = 'http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + symbol + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
             $.getJSON(url, (stockData) => {
                 var stockInfo = stockData.query.results.quote;
@@ -109,8 +92,8 @@ $(document).ready(function() {
 
     // ***USER CAN CHANGE UPDATE TIME***
     var updateTime = $('#update').val()
-    if (isNan(updateTime) == true)
-        setInterval(() =>
-            updateWatchList(), 30000)
+        // if (isNan(updateTime) == true)
+    setInterval(() =>
+        updateWatchList(), 30000)
 });
 // TODO - give user ability to change updatetime
